41a2a76d80494a11ea125fa34ae913d75d89a105
giiTamara <claro.giovana@escola.pr.gov.br>
2025-09-12 11:58:43 -0300

    ofguhegoiregk

--- dom_cherry.js
+++ dom_cherry.js
@@ -5,6 +5,7 @@ rodri_circulo.addEventListener('dblclick',()=>{
 rodri_circulo.style.borderRadius = `${tamanho_rodri}px`;
 rodri_circulo.style.width = `${tamanho_rodri}px`;
 rodri_circulo.style.height = `${tamanho_rodri}px`;
+tamanho_rodri = tamanho_rodri + 20
 
 }
 )
